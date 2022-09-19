42bf67cd14d579f92f4772116a00d4124079fc2c
GaoNeng-wWw <gaonenggg@outlook.com>
2022-09-19 16:51:36 +0800

    🎈 perf(site): 优化打包体积

--- packages/site/vite.config.ts
+++ packages/site/vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import {resolve} from 'path';
 import eslintPlugin from 'vite-plugin-eslint';
+import viteImagemin from 'vite-plugin-imagemin';
+import compression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +11,34 @@ export default defineConfig({
 		vue(),
 		eslintPlugin({
 			include: ['src/**/*.ts', 'src/**/*.vue', 'src/*.ts', 'src/*.vue']
+		}),
+		compression({}),
+		viteImagemin({
+			gifsicle: {
+				optimizationLevel: 7,
+				interlaced: false,
+			},
+			optipng: {
+				optimizationLevel: 7,
+			},
+			mozjpeg: {
+				quality: 20,
+			},
+			pngquant: {
+				quality: [0.8, 0.9],
+				speed: 4,
+			},
+			svgo: {
+				plugins: [
+					{
+						name: 'removeViewBox',
+					},
+					{
+						name: 'removeEmptyAttrs',
+						active: false,
+					},
+				],
+			},
 		})
 	],
 	resolve:{
