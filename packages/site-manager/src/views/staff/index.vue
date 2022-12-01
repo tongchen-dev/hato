bf6465af7602911be48dadab3aa34e3232af42aa
GaoNeng-wWw <gaonenggg@outlook.com>
2022-12-01 11:27:03 +0800

    feat: 移除后端

--- packages/site-manager/src/views/staff/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <a-typography>
-        <a-typography-title :heading="1">
-            Staff
-        </a-typography-title>
-    </a-typography>
-</template>
-
-<script lang="ts" setup></script>
